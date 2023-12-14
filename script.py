a503a944d12efb5d785f0671f2683b9dd09e73ad
Anand Mahamuni <m.anand7203@gmail.com>
2023-12-14 15:48:37 +0530

    Basic desc.

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ import click
 @click.option("--count", default=1, help="Number of greetings.")
 @click.option("--name", prompt="Your name", help="The person to greet.")
 def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
+    """A Simple program that returns LATENCY for <DESTINATION> times."""
     for _ in range(count):
         click.echo(f"Hello, {name}!")
 
